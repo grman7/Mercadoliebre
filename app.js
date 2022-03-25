b2f508982b85e154064e369eef22c98f51bde027
grman7@gmail.com <grman7@gmail.com>
2022-03-24 22:17:16 -0300

    se agrego proyecto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const app = express();
+const PORT = process.env.PORT || 3020;
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=> {
+    res.sendFile(__dirname + "/views/home.html");
+});
+app.get("/register",(req,res)=> {
+    res.sendFile(__dirname + "/views/register.html");
+});
+app.get("/login",(req,res)=> {
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+app.listen (3020,()=> {
+    console.log("Servidor online");
+});
